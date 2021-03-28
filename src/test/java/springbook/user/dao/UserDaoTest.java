f1d3f8cdf11f17c5e75ae2fccbb81256c6aad0de
moong2 <clapmean@gmail.com>
2021-03-28 17:43:49 +0900

    ApplicationContext using xml

--- src/test/java/springbook/user/dao/UserDaoTest.java
+++ src/test/java/springbook/user/dao/UserDaoTest.java
@@ -2,13 +2,14 @@ package springbook.user.dao;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 import springbook.user.domain.User;
 
 import java.sql.SQLException;
 
 public class UserDaoTest {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
         UserDao dao = context.getBean("userDao", UserDao.class);
 
         직접_생성한_DaoFactory_오브젝트_동등성_비교();
