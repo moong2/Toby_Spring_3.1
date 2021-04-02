e2f1fd5a1a7a04f6fb4a421c9963df3774dec1fc
moong2 <clapmean@gmail.com>
2021-04-02 17:31:47 +0900

    TDD & test refactoring

--- src/test/java/springbook/user/dao/UserDaoTest.java
+++ src/test/java/springbook/user/dao/UserDaoTest.java
@@ -1,9 +1,11 @@
 package springbook.user.dao;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.dao.EmptyResultDataAccessException;
 import springbook.user.domain.User;
 
 import java.sql.SQLException;
@@ -13,33 +15,46 @@ import static org.junit.Assert.assertThat;
 
 
 public class UserDaoTest {
-    @Test
-    public void addAndGet() throws ClassNotFoundException, SQLException {
+    private UserDao dao;
+    private User user1;
+    private User user2;
+    private User user3;
+
+    @Before
+    public void setUp() {
         // 1. DaoFactory를 이용한 애플리케이션 컨텍스트
 //        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
 
         // 2. XML을 이용한 애플리케이션 컨텍스트
         ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
-        UserDao dao = context.getBean("userDao", UserDao.class);
+        this.dao = context.getBean("userDao", UserDao.class);
+
+        this.user1 = new User("moong2", "박뭉", "I'm_moong2");
+        this.user2 = new User("chicken", "치킨", "bhc");
+        this.user3 = new User("pizza", "피자", "domino");
+    }
+
+    @Test
+    public void addAndGet() throws ClassNotFoundException, SQLException {
 
         직접_생성한_DaoFactory_오브젝트_동등성_비교();
         스프링컨텍스트로부터_가져온_오브젝트_동등성_비교();
 
-        User user = new User();
-        user.setId("moong2");
-        user.setName("박뭉");
-        user.setPassword("I'm_moong2");
+        dao.deleteAll();
+        assertThat(dao.getCount(), is(0));
 
-        dao.add(user);
+        dao.add(user1);
+        dao.add(user2);
+        assertThat(dao.getCount(), is(2));
 
-        System.out.println(user.getId() + " 등록 성공");
+        User userget1 = dao.get(user1.getId());
+        assertThat(userget1.getName(), is(user1.getName()));
+        assertThat(userget1.getPassword(), is(user1.getPassword()));
 
-        User user2 = dao.get(user.getId());
-
-        assertThat(user2.getName(), is(user.getName()));
-        assertThat(user2.getPassword(), is(user.getPassword()));
+        User userget2 = dao.get(user2.getId());
+        assertThat(userget2.getName(), is(user2.getName()));
+        assertThat(userget2.getPassword(), is(user2.getPassword()));
     }
-
     public static void 직접_생성한_DaoFactory_오브젝트_동등성_비교() {
         DaoFactory factory = new DaoFactory();
         UserDao dao1 = factory.userDao();
@@ -48,7 +63,6 @@ public class UserDaoTest {
         System.out.println(dao1);
         System.out.println(dao2);
     }
-
     public static void 스프링컨텍스트로부터_가져온_오브젝트_동등성_비교() {
         ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
         UserDao dao1 = context.getBean("userDao", UserDao.class);
@@ -56,5 +70,30 @@ public class UserDaoTest {
 
         System.out.println(dao1);
         System.out.println(dao2);
+    }
+
+    @Test
+    public void count() throws ClassNotFoundException, SQLException {
+
+        dao.deleteAll();
+        assertThat(dao.getCount(), is(0));
+
+        dao.add(user1);
+        assertThat(dao.getCount(), is(1));
+
+        dao.add(user2);
+        assertThat(dao.getCount(), is(2));
+
+        dao.add(user3);
+        assertThat(dao.getCount(), is(3));
+    }
+
+    @Test(expected= EmptyResultDataAccessException.class)
+    public void getUserFailure() throws ClassNotFoundException, SQLException {
+
+        dao.deleteAll();
+        assertThat(dao.getCount(), is(0));
+
+        dao.get("unknown_id");
     }
 }
