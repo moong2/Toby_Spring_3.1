37576edbf88e6c92eff2a18077b9455330b127fe
moong2 <clapmean@gmail.com>
2021-04-06 10:02:58 +0900

    JDBC Code with DI

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -72,36 +72,8 @@ public class UserDao {
     }
 
     public void deleteAll() throws SQLException {
-        Connection c = null;
-        PreparedStatement ps = null;
-
-        try {
-            c = dataSource.getConnection();
-            ps = c.prepareStatement("delete from users");
-            ps.executeUpdate();
-        } catch(SQLException e) {
-            throw e;
-        } finally {
-            if(ps != null) {
-                try{
-                    ps.close();
-                }catch(SQLException e){
-
-                }
-            }
-            if(c != null) {
-                try{
-                    c.close();
-                }catch(SQLException e){
-
-                }
-            }
-        }
-
-
-
-        ps.close();
-        c.close();
+        StatementStrategy st = new DeleteAllStatement();
+        jdbcContextWithStatementStrategy(st);
     }
 
     public int getCount() throws SQLException {
@@ -135,6 +107,36 @@ public class UserDao {
                 }
             }
             if(c != null) {
+                try{
+                    c.close();
+                }catch(SQLException e){
+
+                }
+            }
+        }
+    }
+
+    public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException{
+        Connection c = null;
+        PreparedStatement ps = null;
+
+        try {
+            c = dataSource.getConnection();
+
+            ps = stmt.makePreparedStatement(c);
+
+            ps.executeUpdate();
+        } catch(SQLException e){
+            throw e;
+        } finally {
+            if(ps != null) {
+                try{
+                    ps.close();
+                }catch(SQLException e){
+
+                }
+            }
+            if(c != null){
                 try{
                     c.close();
                 }catch(SQLException e){
